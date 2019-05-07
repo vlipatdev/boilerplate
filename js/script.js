befd3f804666e3fda3b74197f6473a9ef1325472
vlipatdev <vlipat.dev@gmail.com>
2019-05-07 17:10:05 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const  = document.querySelector('.');
+const  = document.querySelector('.'); 
+const  = document.querySelector('.');
+const  = document.querySelector('.');
+const  = document.querySelector('.');
